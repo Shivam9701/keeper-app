81939a2af4118a44d31d38361c77a540cb2ed9a4
Shivam Srivastava <84237995+Shivam9701@users.noreply.github.com>
2023-08-29 06:22:01 +0000

    Update 5 files

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./header";
 import Note from "./note";
 import Footer from "./footer";
+import notes from './notes';
 function App() {
   return (
     <div>
